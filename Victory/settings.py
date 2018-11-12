a0af4c878bd7893e1e99400dddebf697b5abf1b0
Phil <44144270+Tristan454@users.noreply.github.com>
2018-11-12 07:48:19 -0600

    Update settings.py

--- Victory/settings.py
+++ Victory/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'uwy@m57mw%t3@jdq9vkrylbkbxc#=d-&ktxp^j+e0eism&3om#'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
